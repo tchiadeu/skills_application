31c8a21db875b2f431c5d5d8c1547165f8c4fd6e
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2025-04-23 12:31:20 +0200

    refacto: passing @skills in private method

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,8 +1,15 @@
 class SkillsController < ApplicationController
+  before_action :set_skills
+
   def index
-    @skills = YAML.load_file("#{Rails.root}/data/skills.yml")
   end
 
   def show
+    @skill = @skills[params[:skill]]
   end
+
+  private
+    def set_skills
+      @skills = YAML.load_file("#{Rails.root}/data/skills.yml")
+    end
 end
