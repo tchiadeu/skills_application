f9362b9947fd01188dcde225198d1fe41cf003e2
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2025-04-23 10:53:26 +0200

    helper: svg_tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def svg_tag(svg_path, options = {})
+    svg = File.open("app/assets/images/#{svg_path}", "rb") { |file| raw file.read }
+    doc = Nokogiri::HTML::DocumentFragment.parse svg
+    svg = doc.at_css "svg"
+    svg["class"] = options.fetch(:class, "")
+    svg["id"] = options.fetch(:id, "")
+    options[:data].each { |key, value| svg["data-#{key.to_s.gsub('_', '-')}"] = value } if options[:data].present?
+    raw svg
+  end
 end
